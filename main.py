2ade5a5d7096dd99bd468e66d477de97a28af2d4
Azo1hz <antoine.diezlatteur@gmail.com>
2023-11-21 13:11:25 +0100

    Version fonctionnelle du simulateur. Exemple d'utilisation dans le main. Crée un fichier txt avec les données (exponentielles complexes). Début de mimo implémenté (à finir)

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import struct
+# import struct
+#
+# print(struct.unpack('f', b'\xdb\x0fI@'))
 
-print(struct.unpack('f', b'\xdb\x0fI@'))
-print('Hello world')
+import simulateur as sml
+
+if __name__ == "__main__":
+
+    Tc = 10 ** -3
+    Te = Tc / 1000
+    T = 64 * Tc
+    fc = 77 * 10 ** 9
+    B = 4 * 10 ** 9
+    simulation = sml.Simulation(T, Tc, Te, fc, B)
+
+    ltx = [[0, 0]]
+    lrx = [[-1, 0], [1, 0]]
+    simulation.board.add(ltx, lrx)
+
+    ltargets = [[0, 5, 0, 0]]
+    simulation.addTrg(ltargets)
+    print(simulation.board.rx)
+    print(simulation.board.tx)
+    print(simulation.targets)
+
+    ts = simulation.process()
+    print(ts)
+
+    xif = simulation.simulation()
+    print(xif)
+
--- simulateur.py
+++ simulateur.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 class Tx:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, virtual=False):
         self.x = x
         self.y = y
+        self.virtual = virtual
 
 
 class Rx:
@@ -33,7 +34,7 @@ class Board:
 
     def add(self, ltx, lrx):  #ltx et lrx de la forme ltx = [[x1, y1], [x2, y2], ...]
         for elt in ltx:
-            self.rx.append(Tx(elt[0], elt[1]))
+            self.tx.append(Tx(elt[0], elt[1]))
 
         for elt in lrx:
             self.rx.append(Rx(elt[0], elt[1]))
@@ -42,8 +43,8 @@ class Board:
 
 class Simulation:
 
-    def __init__(self, T, Te, Tc, fc, B, Mimo=False):
-        self.board = Board
+    def __init__(self, T, Tc, Te, fc, B, Mimo=False):  # attention on veut Te divisible par Tc et Tc divisible par Te
+        self.board = Board()
         self.targets = []
         self.Tc = Tc
         self.Te = Te
@@ -55,9 +56,10 @@ class Simulation:
         self.Mimo = Mimo
         if Mimo:
             # self.board.add()
+            pass
         self.nchan = len(self.board.rx)
 
-    def addtrg(self, ltargets):  #ltargets de la forme ltargets = [[x1, y1, vx1, vy1], [x2, y2, vx2, vy2], ...]
+    def addTrg(self, ltargets):  #ltargets de la forme ltargets = [[x1, y1, vx1, vy1], [x2, y2, vx2, vy2], ...]
         for elt in ltargets:
             self.targets.append(Target(elt[0], elt[1], elt[2], elt[3]))
 
@@ -67,6 +69,7 @@ class Simulation:
             trg.y += trg.vy*self.Tc
 
     def process(self):  # rempli self.τs avec tout les τ associés
+        self.nchan = len(self.board.rx)
         τs = np.zeros((self.nchan, len(self.board.tx) * len(self.targets)))
         i = 0
         for rx in self.board.rx:
@@ -81,29 +84,33 @@ class Simulation:
         return τs
 
     def record(self):
-        ns = self.Tc / self.Te  # nombre de samples par chirp, dimension x
-        nc = self.T / self.Tc  # nombre de de chirps, dimension y
+        ns = int(self.Tc / self.Te)  # nombre de samples par chirp, dimension x
+        nc = int(self.T / self.Tc)  # nombre de de chirps, dimension y
         nrx = self.nchan  # nombre de channels, dimension z
-        xif = np.zeros((ns , nc, nrx))
+        xif = np.zeros((ns, nc, nrx), dtype=complex)
         for j in range(nc):
-            t = np.arange(j*self.Tc, (j+1)*self.Tc+ self.Te, self.Te)
+            t = np.linspace(j*self.Tc, (j+1)*self.Tc, ns)
             τs = self.process()
             self.move()
             for k in range(nrx):
                 for τ in τs[k]:
                     f = τ * self.S
-                    phi = 2 * np.pi * np.fc * τ
-                    xif[0:ns+1, j, k] += np.exp(2 * np.pi * f * t + phi)
+                    phi = 2 * np.pi * self.fc * τ
+                    arg = 2 * np.pi * f * t + phi
+                    xif[0:ns+1, j, k] += np.exp(arg*1j)
 
         return xif
 
     def simulation(self):
         with open('record.txt', 'w') as file:
             xif = self.record()
-            for i in range(len(xif[0, 0])):
-                for j in range(xif[0]):
-                    for k in range(xif):
-                        file.write(xif[i, j, k])
+            ns = len(xif)
+            nc = len(xif[0])
+            nchan = len(xif[0, 0])
+            for k in range(nchan):
+                for j in range(nc):
+                    for i in range(ns):
+                        file.write(f"{xif[i, j, k]} ")
                     file.write('\n')
                 file.write('\n')
 
