b64f39d51575cca46b462ad56ba708a6f3e88e91
amas7 <147492063+amas7@users.noreply.github.com>
2023-12-15 08:44:20 +0100

    Update signalprocessing.py
    affichage fft2d
    et cfar 2d

--- signalprocessing.py
+++ signalprocessing.py
@@ -7,6 +7,28 @@ Created on Fri Nov 17 13:44:35 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import parametre as prm
+import simmu_rdp as simp
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.signal import convolve2d
+from matplotlib import cm
+
+
+tg1 = simp.Target(10,np.deg2rad(0),0,0,1)
+
+tg2 = simp.Target(10,np.deg2rad(45),0,7,1)
+
+tg3 = simp.Target(30,np.deg2rad(-15),0,0.5,0.5)
+
+tg4 = simp.Target(35,np.deg2rad(-60),0,0.2,0.5)
+
+tg5 = simp.Target(40,np.deg2rad(-30),0,-1,0.9)
+
+simu = simp.Simulateur()
+
+simu.run([1,2,3,4],[1,2,3],["DDMA",np.deg2rad(np.array([0,90,270]))],[tg1,tg2])
+
+#
+
 
 
 def TDMA_antenna_2_channel(cube, N_t):
@@ -23,7 +45,11 @@ def TDMA_antenna_2_channel(cube, N_t):
     """
     cube = cube.reshape((cube.shape[0], -1, cube.shape[2]*N_t))
     return cube
-    
+#cube1 = TDMA_antenna_2_channel(cube1,2)
+#cube1=simu.result_channel
+#f2 = np.fft.fft2(cube1,axes = (1,0))
+#f2 = np.fft.fftshift(f2, axes =1)
+#deph = np.rad2deg(np.arange(0,f2.shape[1])*np.pi*2/f2.shape[1])
     
 def calculate_range_fft(cube):
     """Calcule la transformée de Fourier en distance du cube radar.
@@ -35,7 +61,7 @@ def calculate_range_fft(cube):
     - np array de forme (Ns, Nc, Nr*Nt) - le cube transformé de Fourier en distance
     """
     try:
-        fft1 = np.fft.fft(cube, axis=0)  # FFT selon chaque colonne
+        fft1 = np.fft.fft(cube, axis = 0) # FFT selon chaque colonne
     except Exception as e:
         print(f"Erreur: {e}")
         return None
@@ -51,7 +77,6 @@ def plot_range_spectrum(fft1):
     S = prm.S 
     c = prm.c
     Tc = prm.Tc
-    
     fft_abs = np.abs(fft1)
     fft_abs_norm = fft_abs / np.max(fft_abs, axis=0)
     fft_abs_norm_mean = np.mean(fft_abs_norm, axis=(1, 2))  # Moyenne selon les antennes de réception et les chirps
@@ -60,12 +85,12 @@ def plot_range_spectrum(fft1):
     # Affichage des spectres
     fig, axs = plt.subplots(2, 1, figsize=(12, 4))
     # En linéaire
-    axs[0].plot(rang, fft_abs_norm_mean)
+    axs[0].plot(rang,fft_abs_norm_mean)
     axs[0].set_ylabel("Spectre linéaire")
     axs[0].set_title("Spectre linéaire de la transformée de Fourier en distance dans le domaine des distances")
 
     # En dB
-    axs[1].plot(rang, 20 * np.log10(fft_abs_norm_mean))
+    axs[1].plot(rang,20 * np.log10(fft_abs_norm_mean))
     axs[1].set_ylabel("Spectre de puissance (dB)")
     axs[1].set_xlabel("Distance (m)")
 
@@ -81,14 +106,36 @@ def calculate_doppler_fft(fft1):
     - np array de forme (Ns, Nc, Nr*Nt) - le cube transformé de Fourier en vitesse (Doppler)
     """
     try:
-        fft2 = np.fft.fft(fft1, axis=1)  # FFT selon chaque ligne
+        fft2 = np.fft.fft(fft1, axis = 1)
     except Exception as e:
         print(f"Erreur: {e}")
         return None
     
-    return fft2
+    return fft2   
 
-def plot_range_doppler(fft2, mode= [None,None]):
+A = 0
+#cubetd = TDMA_antenna_2_channel(cube1,2)
+#ff1 = calculate_range_fft(cubetd)
+#plot_range_spectrum(ff1)
+def DDMA_antenna2_channel(ff_dop, offset_phase):
+    
+    """Txi__1_Rxj_1....Txi_1__Rxj_n|Txi_2__Rxj_1....Txi_2__Rxj_n|..."""
+    
+    res_chanel = np.zeros((ff_dop.shape[0],ff_dop.shape[1],offset_phase.shape[0]*ff_dop.shape[2] ), dtype = complex)
+    deph = np.arange(0,ff_dop.shape[1])*np.pi*2/ff_dop.shape[1]
+    shilft_array =-np.argmin(np.abs(deph-offset_phase[:,None]), axis = 1)
+    
+    for k in range(shilft_array.shape[0]):
+        res_chanel[:,:,k*ff_dop.shape[2]:(k+1)*ff_dop.shape[2]] = np.roll(ff_dop,shilft_array[k], axis = 1)
+    return res_chanel
+
+def channel_Tx_2_channel_Rx(cube,N_t,N_r):
+    """permet de passer de la respresetation Txi__1_Rxj_1....Txi_1__Rxj_n|Txi_2__Rxj_1....Txi_2__Rxj_n|...
+      à Rxj_1__Txi_1....Rxj_1__Txi_m|Txi_2__Rxj_1....Txi_2__Rxj_n| dans le cube"""
+    indices = np.arange(cube.shape[2]).reshape((N_t, N_r)).T.flatten()
+    return cube[:,:,indices]
+    
+def plot_doppler_spectrum(fft2, mode= [None,None]):
     """Affiche le spectre en vitesse (Doppler) de la transformée de Fourier.
 
     Entrée:
@@ -103,8 +150,17 @@ def plot_range_doppler(fft2, mode= [None,None]):
     nb_tdma_tx = 1
     if mode[0] == 'TDMA':
         nb_tdma_tx = mode[1]
+    elif mode[0] == 'DDMA':
+        
+        fft2 = DDMA_antenna2_channel(fft2, mode[1])
+        Nt =  len(mode[1])
+        Nr = fft2.shape[2]//Nt
         
     fft_abs = np.abs(fft2)
+    if mode[0] == 'DDMA': #conservation du min selon les channels de la meme antenne Tx, pour enlever les pics fantomes
+        fft_abs = fft_abs.reshape((fft_abs.shape[0],fft_abs.shape[1],-1,Nr))
+        fft_abs = np.min(fft_abs, axis = 2).reshape((fft_abs.shape[0],fft_abs.shape[1],Nr))
+           
     fft_abs_norm = fft_abs /np.max(fft_abs, axis=(1,0), keepdims=True)
     fft_abs_norm_mean = np.mean(fft_abs_norm, axis=2) # Moyenne selon les antennes de réception
     fft_abs_norm_mean = np.fft.fftshift(fft_abs_norm_mean, axes=1)
@@ -113,8 +169,98 @@ def plot_range_doppler(fft2, mode= [None,None]):
     
     
     plt.figure(figsize=(8, 6))
+
     plt.imshow(fft_abs_norm_mean,extent=[np.min(speed),np.max(speed),np.min(rang),np.max(rang)], origin = 'bottom')
-    plt.title("2d fft range velocity")
+#    plt.colorbar(label="Power Spectrum (dB)")
+    if mode[0] == "DDMA":
+        plt.title("2d fft range velocity" + " " +mode[0] + " " + str(len(mode[1])) + " " + "antennes TX")
+    elif mode[0] == "TDMA":
+        plt.title("2d fft range velocity" + " " +mode[0] + " " + str(mode[1])+ " " + "antennes TX")
+    else:
+        plt.title("2d fft range velocity SIMO ou SISO" )
     plt.xlabel("Vitesse (m/s)")
     plt.ylabel("range (m)")
+    
+        # Créer un tableau 2D de données
+    data = fft_abs_norm_mean + np.random.rand(fft_abs_norm_mean.shape[0],fft_abs_norm_mean.shape[1])/6
+    
+    # Créer une grille pour les coordonnées x et y
+    x = speed
+    y = rang
+    x, y = np.meshgrid(x, y)
+    
+    # Créer une figure en 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    # Afficher la surface
+    ax.plot_surface(x, y, data, cmap='viridis')
+    
+    # Ajouter des étiquettes aux axes
+    ax.set_xlabel('vitesse (m/s)')
+    ax.set_ylabel('range (m)')
+#    ax.set_zlabel('Axe Z')
+    
+    # Afficher la figure
     plt.show()
+    plt.show()
+    return data
+
+#cube_channel = TDMA_antenna_2_channel(simu.result_true_antenna,3)
+ff_range = calculate_range_fft(simu.result_true_antenna)
+#plot_range_spectrum(ff_range)
+ff_dop = calculate_doppler_fft(ff_range)
+A = plot_doppler_spectrum(ff_dop, ["DDMA",np.deg2rad(np.array([0,90,270]))])
+
+def circular_2d_cfar(falsealmar, nb_guard, nb_reference, ff2d_im):
+    #circulaire
+    mask = np.zeros(((nb_guard+nb_reference)*2+1,(nb_guard+nb_reference)*2+1))
+    x,y = np.meshgrid(np.arange(0,nb_guard+nb_reference+1),np.arange(0,nb_guard+nb_reference+1))
+    V = (x)**2 + (y)**2 <=(((nb_guard+nb_reference)*2+1)/2)**2
+    mask[nb_guard+nb_reference:,nb_guard+nb_reference:] = V
+    mask[:nb_guard+nb_reference,nb_guard+nb_reference:] = V[1:,:][::-1,:]
+    mask[:,:nb_guard+nb_reference] = mask[:,nb_guard+nb_reference+1:][:,::-1]
+    
+    mask[nb_reference:-nb_reference,nb_reference:-nb_reference] = 0
+    
+    #carre
+#    mask = np.ones(((nb_guard+nb_reference)*2+1,(nb_guard+nb_reference)*2+1))
+#    mask[nb_reference:-nb_reference,nb_reference:-nb_reference] = 0
+    
+    N = np.sum(mask) #number of reference cells
+    alpha = N * (falsealmar**(-1/N)-1)
+    mask = mask/N
+    conv = convolve2d(ff2d_im, mask, mode='same', boundary='wrap')*alpha
+    result = conv<ff2d_im
+    return result,conv
+
+detec,conv = circular_2d_cfar(10**-4,1,2,A)
+speed = np.arange(-conv.shape[1]//2,conv.shape[1]//2 )* prm.λ/(2*prm.Tc*conv.shape[1]) 
+rang = np.arange(conv.shape[0]) * 3e8 / (2 * prm.S * prm.Tc)    
+x = speed
+y = rang
+x, y = np.meshgrid(x, y)    
+# Créer une figure en 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Afficher la surface
+ax.plot_surface(x, y, conv, cmap='viridis')
+
+# Ajouter des étiquettes aux axes
+ax.set_xlabel('vitesse (m/s)')
+ax.set_ylabel('range (m)')
+#    ax.set_zlabel('Axe Z')
+
+# Afficher la figure
+plt.show()    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
